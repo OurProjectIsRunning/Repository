4d01803a2786359378100e703d5bd5ed1de4cd08
minseok0415 <kmskorea14@gmail.com>
2023-05-25 12:21:32 +0900

    메인 컴포넌트 만듦

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+function Main() {
+    return (
+        <div className="Main">
+            <h1>메인 페이지 들어갈 예정
+                공사중
+            </h1>
+        </div>
+    )
+}
+
+export default Main;
