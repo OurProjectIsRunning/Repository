e9f5a1fa773a1f5fe0e4a29496b9ba52a1cc19fe
Deterism <37361885+chickenchi@users.noreply.github.com>
2023-05-25 12:10:22 +0900

    Mw :: Add Gain Component

---
+++ src/components/Gain.js
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+        <div className="Gain">
+            <img src="./social_profile.png" />
+            <p>Deterism</p>
+            <p>아잉의 음운의 개수는 3개</p>
+            <hr />
+            <a>글</a>
+            <a>시리즈</a>
+            <a>소개</a>
+            <input type="text"></input>
+            <a>태그 목록</a>
+            <br />
+            <a>전체보기 (0)</a>
+        </div>
+    )
+}
+
+export default Header;
