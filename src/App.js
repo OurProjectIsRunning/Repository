77dbf12d11079735758249d7acc660be74a57803
Deterism <37361885+chickenchi@users.noreply.github.com>
2023-06-01 10:03:38 +0900

    Divide

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './style.scss';
-import Header from './components/Header'
+import Header from './components/Header';
+import Section from './components/Section';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Section />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Gain from './Gain';
-import Main from './Main';
-
 function Header() {
     return (
         <div className="Header">
             <h1>아잉</h1>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/Gain" element={<Gain />}></Route>
-                    <Route path="/Main" element={<Main />}></Route>
-                </Routes>
-            </BrowserRouter>
         </div>
     )
 }
